2e0bbd273f81cbb638166f9eeb57b5a60fafdccb
Sam Wilson <wilsosam@iu.edu>
2020-04-23 17:27:14 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
   return (
